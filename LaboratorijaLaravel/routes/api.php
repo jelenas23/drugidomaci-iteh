917f69dc64cd77b96c3e5f03e3c84f66c9f38f73
jelenas23 <js20190188@student.fon.bg.ac.rs>
2023-01-10 13:59:26 +0100

    Dodata ruta za login

--- LaboratorijaLaravel/routes/api.php
+++ LaboratorijaLaravel/routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::resource('laboratorija',LaboratorijaController::class);
 Route::resource('laborant', LaborantController::class);
